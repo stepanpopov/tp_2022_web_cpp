e7081dcaa06efcbb208e58f8758e9cf75e223d10
stepan <jomolun@yandex.com>
2022-11-27 16:54:08 +0300

    done set funcs without iterators

--- include/set.h
+++ include/set.h
@@ -1,78 +1,105 @@
 #pragma once // NOLINT
 
-#include <functional>
-#include <limits>
-#include <vector>
-#include <initializer_list>
-#include <iomanip>
+// #include <functional>
+// #include <limits>
+#include <vector>  // out
+// #include <initializer_list>
+// #include <iomanip>
 #include <iostream>
 
 namespace tp {
 
-    template<class T>
-    struct node {
-        node() : key(T()) {}
+template<class T>
+struct node {
+    node() : key(T()) {}
 
-        node(const T &key) : key(key) {}
+    node(const T &key) : key(key) {}
 
-        T key;
-        size_t height = 1;
-        node *parent = nullptr;
-        node *right = nullptr;
-        node *left = nullptr;
-        // node* _next = nullptr;
-        // node* _prev = nullptr;
-    };
+    T key;
+    size_t height = 1;
+    node *parent = nullptr;
+    node *right = nullptr;
+    node *left = nullptr;
+    // node* _next = nullptr;
+    // node* _prev = nullptr;
+};
 
-    template<class T>
-    class set {
-    public:
-        set();
+template<class T>
+class set {
+public:
+    set();
 
-        set(const set<T> &s);
+    set(const set<T> &s);
 
-        set<T> &operator=(const set<T> &s);
+    set<T> &operator=(const set<T> &s);
 
-        ~set();
+    ~set();
 
-        void insert(const T &key);
+    void insert(const T &key);
 
-        void erase(const T &key);
+    void erase(const T &key);
 
-        size_t get_size() const;
+    size_t get_size() const;
 
-        bool empty() const;
+    bool empty() const;
 
-    private:
-        node<T> *node_insert(node<T> *n, const T &key);
+    friend
+    std::ostream &operator<<(std::ostream &out, const set<T> &s) {
+        out << "size : " << s.size << std::endl;
+        if (s.root != nullptr)
+            out << "root_key : " << s.root->key << std::endl;
+        std::vector<T> vec;
+        s.vector_from_set_dfs_preorder(s.root, vec);
+        for (auto it: vec) {
+            out << it << " ";
+        }
+        out << std::endl;
+        return out;
+    }
 
-        node<T> *node_find_min(node<T> *n);
+private:
+    void vector_from_set_dfs_preorder(node<T> *n, std::vector<T> &v) const {
+        if (n == nullptr) {
+            return;
+        }
+        v.push_back(n->key);
+        vector_from_set_dfs_preorder(n->left, v);
+        vector_from_set_dfs_preorder(n->right, v);
+    }
 
-        node<T> *node_without_min(node<T> *n);
+    void node_visit_and_delete(node<T> *n);
 
-        node<T> *node_remove(node<T> *n, const T &key);
+    void node_visit_and_copy(node<T> *&n, node<T> *copy_node);
 
-        size_t node_balance_factor(node<T> *n);
+    bool node_find(node<T> *n, const T &key) const;
 
-        void node_fix_height(node<T> *n);
+    node<T> *node_insert(node<T> *n, const T &key);
 
-        size_t node_height(node<T> *n);
+    node<T> *node_find_min(node<T> *n) const;
 
-        node<T> *node_rotate_right(node<T> *n);
+    node<T> *node_without_min(node<T> *n);
 
-        node<T> *node_rotate_left(node<T> *n);
+    node<T> *node_remove(node<T> *n, const T &key);
 
-        node<T> *node_balance(node<T> *n);
+    int node_balance_factor(node<T> *n) const;
+
+    void node_fix_height(node<T> *n);
+
+    size_t node_height(node<T> *n) const;
+
+    node<T> *node_rotate_right(node<T> *n);
+
+    node<T> *node_rotate_left(node<T> *n);
+
+    node<T> *node_balance(node<T> *n);
 
 
-        node<T> *root = nullptr;
-        node<T> *begin = nullptr;
-        node<T> *last = nullptr;
+    node<T> *root = nullptr;
+    node<T> *begin = nullptr;
+    node<T> *last = nullptr;
 
-        size_t size = 0;
-    };
-
-    // std::ostream &operator<<(std::ostream &os, const set<T> &s);
+    size_t size = 0;
+};
 }  // namespace tp
 
 #include "set.impl"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #include "set.h"
 
+using namespace std;
+
 int main() {
-    tp::set<int> s;
+    tp::set<char> s;
+    // cout << s;
+    // char a = 'a';
+    s.insert('a');
+    s.insert('b');
+    s.insert('c');
+    s.insert('d');
+    s.insert('e');
+    s.insert('h');
+    s.insert('f');
+    s.insert('g');
+
+    cout << s;
+    /*for(int i = 1; i < 6; ++i) {
+        s.insert(i);
+    }
+    s.insert(3);
+    // size 5
+    *//*s.erase(4);
+    s.erase(1);
+    s.erase(3);
+    s.erase(2);
+    s.erase(5);
+    s.erase(5);*//*
+    auto s1(s);
+    cout << s1;
+    s1.insert(10);
+    s1.erase(4);
+    s1.erase(1);
+    s1.erase(3);
+    s1.erase(2);
+    s1.erase(5);
+    s1.erase(10);
+    cout << endl;
+    cout << s1;*/
 
     return 0;
 }
