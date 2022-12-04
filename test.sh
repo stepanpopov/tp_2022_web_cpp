01d750dc7b5aaa8155b1bb5ee9e66543e52891c5
stepan <jomolun@yandex.com>
2022-12-04 03:21:10 +0300

    more tests added

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/bash
+
+PROJECT_DIR=$(pwd)
+
+function print_header() {
+    echo -e "\n***** ${1} *****"
+}
+
+print_header "RUNNING TESTS"
 cd ./build && ctest -V -R
 
-# echo $PWD
-# lcov -t "tests/test_base" -o coverage.info -c -d matrix
-# genhtml -o report coverage.info
+print_header "GENERATING COVERAGE INFO"
+lcov -o coverage.info -c -d tests
+lcov --remove coverage.info '/usr/*' "${PROJECT_DIR}/tests/*" -o filtered_coverage.info
+
+print_header "COVERAGE REPORT"
+genhtml -o report filtered_coverage.info
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
-
 #include "set.hpp"
+
+#include <set>
+#include <vector>
 
 TEST(TestSet, correct_empty_iterators) {
     tp::Set<int> s;
-    for(int i = 1; i < 10; ++i) {
+    for (int i = 1; i < 10; ++i) {
         s.insert(i);
     }
 
-    for(int i = 1; i < 10; ++i) {
+    for (int i = 1; i < 10; ++i) {
         s.erase(i);
     }
     ASSERT_TRUE(s.begin() == s.end());
@@ -18,14 +20,72 @@ TEST(TestSet, correct_empty_iterators) {
 
 TEST(TestSet, correct_empty_lower_bound) {
     tp::Set<int> s;
-    for(int i = 1; i < 10; ++i) {
-    s.insert(i);
+    for (int i = 1; i < 10; ++i) {
+        s.insert(i);
     }
 
-    for(int i = 1; i < 10; ++i) {
-    s.erase(i);
+    for (int i = 1; i < 10; ++i) {
+        s.erase(i);
     }
     ASSERT_EQ(s.lower_bound(13), s.begin());
 }
 
+TEST(TestSet, lower_bound_check_for_existed_value) {
+    tp::Set<char> s;
+    s.insert('a');
+    s.insert('c');
+    s.insert('b');
+    s.insert('f');
+    s.insert('i');
+    s.insert('j');
+    s.insert('u');
 
+    ASSERT_EQ(s.lower_bound('c'), ++(++s.begin()));
+}
+
+class TestComparingWithStl : public ::testing::Test {
+public:
+    void SetUp() override {
+        for (size_t i = 0; i < START_SIZE; ++i) {
+            init_elements.push_back(rand() % 1000);
+            my_set.insert(init_elements[i]);
+            stl_set.insert(init_elements[i]);
+        }
+    }
+
+    void TearDown() override {
+
+    }
+
+    tp::Set<int> my_set;
+    std::set<int> stl_set;
+
+    std::vector<int> init_elements;
+
+    static const size_t START_SIZE = 1000000;
+};
+
+TEST_F(TestComparingWithStl, erase_test) {
+    for (size_t i = 0; i < START_SIZE; ++i) {
+        my_set.erase(init_elements[i]);
+        stl_set.erase(init_elements[i]);
+    }
+
+    auto it1 = my_set.begin();
+    auto it2 = stl_set.begin();
+    for (; it1 != my_set.end() && it2 != stl_set.end();
+           ++it1, ++it2) {
+        ASSERT_EQ(*it1, *it2);
+    }
+}
+
+TEST_F(TestComparingWithStl, find_test) {
+    for (size_t i = 0; i < START_SIZE; ++i) {
+
+        auto it1_found = my_set.find(init_elements[i]);
+        auto it2_found = stl_set.find(init_elements[i]);
+
+        ASSERT_TRUE((it1_found == my_set.end() && it2_found == stl_set.end()) ||
+                    (*it1_found == *it2_found));
+    }
+}
